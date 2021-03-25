e021483d0e3fd9ae47f51ed850a00cc750211abb
Xavier-Frank <oduorfrancis134@gmail.com>
2021-03-25 13:13:32 +0300

    first front-end + backend integration

---
+++ src/main/java/com/example/realestate/RealestatesIiApplication.java
@@ -0,0 +1,13 @@
+package com.example.realestate;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RealestatesIiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RealestatesIiApplication.class, args);
+	}
+
+}
